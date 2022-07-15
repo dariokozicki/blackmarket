938318555f63f4a74dbd07fc7ab9c1e6172f5877
Dario Kozicki <dariokozicki@gmail.com>
2022-07-15 11:10:13 -0300

    Add hot reload and dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 /* istanbul ignore file */
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
