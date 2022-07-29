5aba32c86d5652a1d737ff82668e60dba695df54
Dario Kozicki <dariokozicki@gmail.com>
2022-07-29 16:42:17 -0300

    remove coverage files, add to gitignore, add delete test

--- test/unit/products/services/products.service.spec.ts
+++ test/unit/products/services/products.service.spec.ts
@@ -8,9 +8,11 @@ import { ProductsService } from '@products/services/products.service';
 describe('Products Service', () => {
   let service: ProductsService;
   let find: jest.Mock;
+  let del: jest.Mock;
 
   beforeEach(async () => {
     find = jest.fn();
+    del = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ProductsService,
@@ -18,6 +20,7 @@ describe('Products Service', () => {
           provide: getRepositoryToken(Product),
           useValue: {
             find,
+            delete: del,
           },
         },
       ],
@@ -29,30 +32,42 @@ describe('Products Service', () => {
   it('Should be defined', () => {
     expect(service).toBeDefined();
   });
+  describe('FindAll', () => {
+    it('Should return array of products if data sent is correct', async () => {
+      const prod = new Product();
+      prod.name = 'Lapicera';
+      const categoryId = 3;
+      const categoryProducts = new CategoryProducts();
+      categoryProducts.category = new Category();
+      categoryProducts.category.id = categoryId;
+      prod.categoryProducts = [new CategoryProducts()];
+      find.mockReturnValueOnce(Promise.resolve([prod]));
+      const [theProd] = await service.findAll({
+        page: 1,
+        size: 30,
+        categories: [categoryId],
+        search: 'Lapi',
+      });
+      expect(theProd.name).toBe(prod.name);
 
-  it('Should return array of products if data sent is correct', async () => {
-    const prod = new Product();
-    prod.name = 'Lapicera';
-    const categoryId = 3;
-    const categoryProducts = new CategoryProducts();
-    categoryProducts.category = new Category();
-    categoryProducts.category.id = categoryId;
-    prod.categoryProducts = [new CategoryProducts()];
-    find.mockReturnValueOnce(Promise.resolve([prod]));
-    const [theProd] = await service.findAll({
-      page: 1,
-      size: 30,
-      categories: [categoryId],
-      search: 'Lapi',
+      find.mockReturnValueOnce(Promise.resolve([prod]));
+      const [secondProd] = await service.findAll({
+        page: 1,
+        size: 30,
+        categories: [categoryId],
+      });
+      expect(secondProd.name).toBe(prod.name);
     });
-    expect(theProd.name).toBe(prod.name);
+  });
 
-    find.mockReturnValueOnce(Promise.resolve([prod]));
-    const [secondProd] = await service.findAll({
-      page: 1,
-      size: 30,
-      categories: [categoryId],
+  describe('Delete', () => {
+    it('Should call repository.delete with productId', async () => {
+      const prodId = 1;
+      const aff = 1;
+      del.mockReturnValueOnce({ affected: aff, raw: {} });
+      const { affected } = await service.delete(1);
+      expect(affected).toBe(aff);
+      expect(del).toHaveBeenCalledWith({ id: prodId });
     });
-    expect(secondProd.name).toBe(prod.name);
   });
 });
